2c108402e1632a97ae655e9073872b43a8b8289d
artrela <trelaalec@gmail.com>
2024-04-05 13:10:53 -0400

    started pipeline: skeleton

--- PyVO/PyVO/scripts/visual_odometry.py
+++ PyVO/PyVO/scripts/visual_odometry.py
@@ -3,50 +3,92 @@ import rclpy
 from rclpy.node import Node
 
 import numpy as np
-# TODO: include needed ROS msg type headers and libraries
-from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
-from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from typing import List
 
+from message_filters import TimeSynchronizer, Subscriber
+from sensor_msgs.msg import Image
 
-class SafetyNode(Node):
+class VisualOdometry(Node):
     """
-    The class that handles emergency braking.
+    The class that handles the VO pipeline
     """
     def __init__(self):
-        super().__init__('safety_node')
+        super().__init__('visual_odom')
         """
-        One publisher should publish to the /drive topic with a AckermannDriveStamped drive message.
-
-        You should also subscribe to the /scan topic to get the LaserScan messages and
-        the /ego_racecar/odom topic to get the current speed of the vehicle.
-
-        The subscribers should use the provided odom_callback and scan_callback as callback methods
-
-        NOTE that the x component of the linear velocity in odom is the speed
         """
-        self.speed = 0.
-        # TODO: create ROS subscribers and publishers.
 
-    def odom_callback(self, odom_msg):
-        # TODO: update current speed
-        self.speed = 0.
+        self.detector = None
+        self.instrinsic_matrix = None
 
-    def scan_callback(self, scan_msg):
-        # TODO: calculate TTC
+        self.trajectory: List[np.ndarray] = [np.eye(4)]
+        self.color_prev = None
+
+        self.load_config("")
+
+        tss = TimeSynchronizer(Subscriber("/camera/depth/image_rect_raw", Image),
+                       Subscriber("/camera/color/image_raw", Image))
+        tss.registerCallback(self.visual_odom_callback)
+
+
+    def visual_odom_callback(self, color_img: Image, depth_img: Image):
+        """
+        run VO pipeline
+        """
+
+        if self.color_prev is None:
+            self.color_prev = color_img
+            return
         
-        # TODO: publish command to brake
+        kps1, desc1 = self.get_features(self.color_prev, self.detector)
+        kps2, desc2 = self.get_features(color_img, self.detector)
+
+        matches = self.get_matches(desc1, desc2, self.detector)
+        
+        relative_transform = self.motion_estimate(kps1, kps2, matches, depth_img)
+        global_transform = self.trajectory[-1] @ relative_transform
+        
+        self.trajectory.append(global_transform)
+        self.visualize_trajectory(self.trajectory)
+
+        self.color_prev = color_img
+
+
+    def get_features(self, image, detector):
+        """
+        get features from image
+        """
+        pass
+    
+    def get_matches(self, descriptors1, descriptors2, detector):
+        """"
+        match features between two frames
+        """
+        
+        pass
+    
+    def motion_estimate(self, kp_t, kp_tnext, matches, depth_img):
+        pass
+
+
+    def visualize_trajectory(self, trajectory: List[np.ndarray]):
+        """_summary_
+
+        Args:
+            trajectory (List[np.ndarray]): _description_
+        """
+        
+
+    def load_config(self, path):
         pass
 
 def main(args=None):
+
     rclpy.init(args=args)
-    safety_node = SafetyNode()
-    rclpy.spin(safety_node)
+    vo_node = VisualOdometry()
+    rclpy.spin(vo_node)
 
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    safety_node.destroy_node()
+    vo_node.destroy_node()
     rclpy.shutdown()
 
 

8af422f1217eb616e2eb61c9646c872ccf6e4944
sridevikaza <sridevikaza98@gmail.com>
2024-04-10 19:55:18 -0400

    motion estimate and visualization functions filled out, not yet tested

--- src/cpp_vo/include/cpp_vo/vo.h
+++ src/cpp_vo/include/cpp_vo/vo.h
@@ -14,8 +14,16 @@
 #include "nav_msgs/msg/path.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <Eigen/Geometry>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/core/eigen.hpp> 
 
 
 using namespace std;
@@ -30,23 +38,26 @@ class VO : public rclcpp::Node {
     // initializations
     cv::Mat color_prev, depth_prev;  
     cv::Mat intrinsics;
+    visualization_msgs::msg::MarkerArray pose_markers;
+    nav_msgs::msg::Path traj_path;
 
     // publishers and subscribers
-
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr poses_viz_pub;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr traj_viz_pub;
 
     // callback
     void visual_odom_callback(const sensor_msgs::msg::Image::ConstSharedPtr depth_msg,
         const sensor_msgs::msg::Image::ConstSharedPtr color_msg);
 
     // methods
-    cv::Mat convert_image(const sensor_msgs::msg::Image& image_msg, bool depth);
+    cv::Mat convert_image(const sensor_msgs::msg::Image::ConstSharedPtr image_msg, bool depth);
     void draw_matches(const cv::Mat& img1, const cv::Mat& img2, const vector<cv::KeyPoint>& kps1, 
         const vector<cv::KeyPoint>& kps2, const vector<cv::DMatch>& matches);
     void get_features();
     void get_matches();
     cv::Mat motion_estimate(const vector<cv::KeyPoint>& kp_tprev, const vector<cv::KeyPoint>& kp_t,
         const vector<cv::DMatch>& matches, const cv::Mat& depth_t_prev);
-    void visualize_trajectory();
+    void visualize_trajectory(cv::Mat tf);
     void load_config(string filepath);
 
 
--- src/cpp_vo/src/vo.cpp
+++ src/cpp_vo/src/vo.cpp
@@ -1,4 +1,5 @@
 #include "cpp_vo/vo.h"
+
 using namespace std;
 using sensor_msgs::msg::Image;
 using namespace message_filters;
@@ -18,15 +19,22 @@ VO::VO(): rclcpp::Node("vo_node")
     auto sync = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(
             SyncPolicy(10), depth_img_sub, color_img_sub);
     sync->registerCallback(std::bind(&VO::visual_odom_callback, this, std::placeholders::_1, std::placeholders::_2));
+    poses_viz_pub = this->create_publisher<visualization_msgs::msg::MarkerArray>("/poses_viz", 10);
+    traj_viz_pub = this->create_publisher<nav_msgs::msg::Path>("/traj_viz", 10);
 
     // initialize tf values
     cv::Mat global_tf = cv::Mat::eye(4, 4, CV_64F);
-    global_tf.col(3).setTo(cv::Scalar(1));
 
     // set intrinsics values
     intrinsics = (cv::Mat_<double>(3, 3) <<  606.328369140625, 0, 322.6350402832031,
                                             0, 605.257568359375, 239.6647491455078,
                                             0.0, 0.0, 1.0);
+
+    // visualizing pose chain
+    pose_markers = visualization_msgs::msg::MarkerArray();
+    traj_path = nav_msgs::msg::Path();
+    traj_path.header.frame_id = "origin";
+    visualize_trajectory(global_tf);
 
     RCLCPP_INFO(rclcpp::get_logger("VO"), "%s\n", "Created new VO Object.");
 }
@@ -48,8 +56,7 @@ void VO::visual_odom_callback(const sensor_msgs::msg::Image::ConstSharedPtr dept
     // update prev images
 }
 
-cv::Mat VO::convert_image(const sensor_msgs::msg::Image& image_msg, bool depth){
-
+cv::Mat VO::convert_image(const sensor_msgs::msg::Image::ConstSharedPtr image_msg, bool depth){
 }
 
 void VO::draw_matches(const cv::Mat& img1, const cv::Mat& img2, const vector<cv::KeyPoint>& kps1, const vector<cv::KeyPoint>& kps2, const vector<cv::DMatch>& matches){
@@ -69,48 +76,86 @@ void VO::get_matches(){
 cv::Mat VO::motion_estimate(const vector<cv::KeyPoint>& kp_tprev, const vector<cv::KeyPoint>& kp_t,
     const vector<cv::DMatch>& matches, const cv::Mat& depth_t_prev)
 {
-    // cv::Mat tf = cv::Mat::eye(4, 4, CV_64F);
+    cv::Mat tf = cv::Mat::eye(4, 4, CV_64F);
 
-    // if (matches.size() < 4) {
-    //     RCLCPP_INFO(rclcpp::get_logger("VO"), "Less than 4 matches! Cannot attempt PnP");
-    //     return tf;
-    // }
+    // check for at least 4 matches
+    if (matches.size() < 4) {
+        RCLCPP_INFO(rclcpp::get_logger("VO"), "Less than 4 matches! Cannot attempt PnP");
+        return tf;
+    }
 
-    // vector<cv::Point2f> kp_tprev_idx, kp_t_idx;
-    // for (const auto& m : matches) {
-    //     kp_tprev_idx.push_back(kp_tprev[m[0].queryIdx].pt);
-    //     kp_t_idx.push_back(kp_t[m[0].trainIdx].pt);
-    // }
+    std::vector<cv::Point2f> kp_tprev_idx, kp_t_idx;
+    std::vector<cv::Point3f> world_pts;
 
-    // vector<double> Z;
-    // for (const auto& point: kp_tprev_idx){
-    //     auto value = depth_t_prev.at<double>(point.x, point.y);
-    //     z.push_back(value);
-    // }
+    for (auto& match : matches) {
 
-    // vector<Point3f> world_pts(Z.size());
-    // vector<Point3f> img_pts(Z.size());
+        // get x,y coordinates of matched keypoints
+        cv::Point2f pt_prev = kp_tprev[match.queryIdx].pt;
+        cv::Point2f pt_current = kp_t[match.trainIdx].pt;
 
-    // for (int i=0; i<Z.size(); ++i){
-
-    // }
-
-    // cv::Mat rvec, tvec;
-    // cv::Mat dist_coeffs = cv::Mat::zeros(4, 1, CV_64F); // assuming no lens distortion
-    // bool success = cv::solvePnPRansac(world_pts, img_pts, intrinsics, dist_coeffs, rvec, tvec);
-
-    // if (success) {
+        // get depth value at keypoint coords
+        auto Z = depth_t_prev.at<double>(pt_prev.y, pt_prev.x);
         
-    // } else {
-    //     RCLCPP_INFO(rclcpp::get_logger("VO"), "PnP Pose Estimate Failed!");
-    // }
+        // get world coordinates from intrinsics
+        float X = Z * (pt_prev.x - intrinsics.at<double>(0, 2)) / intrinsics.at<double>(0, 0);
+        float Y = Z * (pt_prev.y - intrinsics.at<double>(1, 2)) / intrinsics.at<double>(1, 1);
 
-    // return tf;
+        world_pts.push_back(cv::Point3f(X, Y, Z));
+        kp_t_idx.push_back(pt_current);
+
+    }
+
+    // use solve PnP RANSAC to get rvec and tvec
+    cv::Mat rvec, tvec;
+    bool success = cv::solvePnPRansac(world_pts, kp_t_idx, intrinsics, cv::Mat(), rvec, tvec);
+
+    // make tf matrix
+    if (success) {
+        cv::Mat R;
+        cv::Rodrigues(rvec, R);
+        R.copyTo(tf(cv::Rect(0, 0, 3, 3)));
+        tvec.copyTo(tf(cv::Rect(3, 0, 1, 3)));
+    } else {
+        RCLCPP_INFO(rclcpp::get_logger("VO"), "PnP Pose Estimate Failed");
+    }
+
+    return tf;
 
 }
 
 
-void VO::visualize_trajectory(){
+void VO::visualize_trajectory(cv::Mat tf){
+    Eigen::Matrix4d eigen_mat;
+    cv::cv2eigen(tf, eigen_mat);
+    Eigen::Isometry3d eigen_tf = Eigen::Isometry3d(eigen_mat);
+    Eigen::Quaterniond eigen_quat(eigen_tf.rotation());
+
+    auto pose_marker = visualization_msgs::msg::Marker();
+    pose_marker.header.frame_id = "origin";
+    pose_marker.id = pose_markers.markers.size()+1;
+    pose_marker.type = 0;
+    pose_marker.pose.position.x = eigen_tf.translation().x();
+    pose_marker.pose.position.y = eigen_tf.translation().y();
+    pose_marker.pose.position.z = eigen_tf.translation().z();
+    pose_marker.pose.orientation.x = eigen_quat.x();
+    pose_marker.pose.orientation.y = eigen_quat.y();
+    pose_marker.pose.orientation.z = eigen_quat.z();
+    pose_marker.pose.orientation.w = eigen_quat.w();
+    pose_marker.scale.x = 0.3;
+    pose_marker.scale.y = 0.025;
+    pose_marker.scale.z = 0.025;
+    pose_marker.color.a = 1.0;
+    pose_marker.color.b = 1.0;
+    pose_markers.markers.push_back(pose_marker);
+
+    auto traj_marker = geometry_msgs::msg::PoseStamped();
+    traj_marker.pose.position.x = eigen_tf.translation().x();
+    traj_marker.pose.position.y = eigen_tf.translation().y();
+    traj_marker.pose.position.z = eigen_tf.translation().z();
+    traj_path.poses.push_back(traj_marker);
+
+    poses_viz_pub->publish(pose_markers);
+    traj_viz_pub->publish(traj_path);
 
 }
 

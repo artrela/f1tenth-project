4a7b9de0d2ac2a02e16149b69406129eb5a1222f
sridevikaza <sridevikaza98@gmail.com>
2024-04-19 15:49:44 -0400

    can't divide depth in python world

--- src/py_vo/scripts/visual_odometry.py
+++ src/py_vo/scripts/visual_odometry.py
@@ -174,7 +174,7 @@ class VisualOdometry(Node):
 
         #TODO depth clipping???
         Z = depth_t_prev[kp_tprev_idx[:, 1], kp_tprev_idx[:, 0]]
-        Z /= 1000
+        # Z /= 1000
         # low_depth = Z < 6000
 
         # Z = Z[low_depth]
@@ -255,7 +255,7 @@ class VisualOdometry(Node):
         """
         # cwd = os.getcwd()
         # package_share_directory = get_package_share_directory('py_vo')
-        yaml_file_path = os.path.join("/home/atrela/Documents/f1tenth-project/src/py_vo", "config", filename)
+        yaml_file_path = os.path.join("/home/sridevi/Documents/f1tenth-project/src/py_vo/config/vo_config.yaml")
 
         with open(yaml_file_path, 'r') as file:
             data = yaml.safe_load(file)
